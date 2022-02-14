045ff250bd1e695165cee48f0ce8b953e80ed274
khhhrystyyna <khrystyna.styslo@gmail.com>
2022-02-14 23:19:35 +0200

    js base 3

---
+++ script.js
@@ -0,0 +1,71 @@
+// const city = prompt("What is your city?", "Lviv")
+//
+// switch (city) {
+//     case "Lviv": {
+//         alert("Your city is Lviv")
+//         break
+//     }
+//     case "Kyiv": {
+//         alert("Your city is Kyiv")
+//         break
+//     }
+//     case "Odesa": {
+//         alert("Your city is Odesa")
+//         break
+//     }
+//     default: {
+//         alert("I don't know your city")
+//     }
+// }
+
+
+// for (початок; умова; крок) {
+//   тіло циклу
+// }
+
+// let sum = 0
+//
+// for (let i = 0; i < 5; i++) {
+//     sum += i
+//
+//     for (let j = 0; j <= 3; j++) {
+//         console.log('i: ', i, 'j: ', j)
+//     }
+// }
+//
+// console.log('sum: ', sum)
+
+// let i = 0
+// Аналогічно до for
+// while (i <= 5) {
+//     console.log('i: ', i)
+//
+//     i++
+// }
+
+// let i = 0
+//
+// do {
+//     console.log('i: ', i)
+//     i++
+// } while (i < 0)
+
+// const name = "Khrystyna"
+
+// const result = "My name is " + name
+// OR
+// const result = `My name is ${name}`
+//
+// console.log('result ',  result)
+
+let sum = 2
+
+for (let i = 1; i <= 20; i++) {
+    console.log('sum: ', sum)
+    sum += sum
+}
+
+
+// number1
+// number2
+
